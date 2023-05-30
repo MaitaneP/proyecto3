12c25cfc7d29c3a5cb0ff7c5394569b5e121ecf1
MaitaneP <portillamaitane@gmail.com>
2023-05-30 13:51:25 +0200

    ruta style

--- web/app.py
+++ web/app.py
@@ -38,7 +38,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("style/style.css")
+local_css("web/style/style.css")
 
 
 # sección encabezado y logo a dos columnas
